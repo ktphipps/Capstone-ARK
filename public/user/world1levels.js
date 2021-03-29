80bb15649f04fda0237bbe866e83e9dea16d016c
waramsey <waramsey@unomaha.edu>
2021-03-29 15:49:54 -0600

    Implemented Star Rating (front end)

--- public/user/world1levels.js
+++ public/user/world1levels.js
@@ -14,9 +14,37 @@ firebase.auth().onAuthStateChanged(async function (user) {
 
       if (docSnapshot.exists)
         worldTotal = docSnapshot.get("world1Total");
-      
+        
+      let lvlRatingRef = [];
+      let docSnap = [];
+      let numStars = [];
+
+      for (let i = 0; i < 11; i++) {
+        lvlRatingRef.push(firebase.firestore().collection('users').doc(user.uid).collection("highscores").doc("World1Level"+i));
+        docSnap.push(await lvlRatingRef[i].get());
+  
+        if (docSnap[i].exists) {
+          numStars.push(docSnap[i].get("rating"));
+        }
+      }
+
+
       $("#stars").text(function () {
         return $(this).text().replace("...", worldTotal); 
       });
+      
+      $(".stars").append(function () {
+        let levelNum = $(this).attr('id').match(/\d+/)[0];
+        let images = []
+        
+        for (let i = 0; i < 3; i++) {
+          if (numStars[levelNum] !== undefined && i < numStars[levelNum]) {
+            images.push("<img id='"+i+"' src='./game/assets/star.png'/>")
+          } else {
+            images.push("<img id='"+i+"' src='./game/assets/emptyStar.png'/>")
+          }
+        }
+        return $(this).html(images[0] + images[1] + images[2])
+      })
     }
 });
--- public/user/world3levels.js
+++ public/user/world3levels.js
@@ -1,22 +1,50 @@
 firebase.auth().onAuthStateChanged(async function (user) {
-    // If user is not signed in
-    if(!user)
-    {
-      console.log("You are not signed in.");
-      window.location = "/login.html";
-    }
-    else // User is signed in 
-    {
-      let ratingRef = firebase.firestore().collection('users').doc(user.uid).collection("ratings").doc("RatingTotals");
-      let docSnapshot = await ratingRef.get();
-      
-      var worldTotal = 0;
+  // If user is not signed in
+  if(!user)
+  {
+    console.log("You are not signed in.");
+    window.location = "/login.html";
+  }
+  else // User is signed in 
+  {
+    let ratingRef = firebase.firestore().collection('users').doc(user.uid).collection("ratings").doc("RatingTotals");
+    let docSnapshot = await ratingRef.get();
+    
+    var worldTotal = 0;
 
-      if (docSnapshot.exists)
-        worldTotal = docSnapshot.get("world3Total");
+    if (docSnapshot.exists)
+      worldTotal = docSnapshot.get("world3Total");
       
-      $("#stars").text(function () {
-        return $(this).text().replace("...", worldTotal); 
-      });
+    let lvlRatingRef = [];
+    let docSnap = [];
+    let numStars = [];
+
+    for (let i = 0; i < 11; i++) {
+      lvlRatingRef.push(firebase.firestore().collection('users').doc(user.uid).collection("highscores").doc("World3Level"+i));
+      docSnap.push(await lvlRatingRef[i].get());
+
+      if (docSnap[i].exists) {
+        numStars.push(docSnap[i].get("rating"));
+      }
     }
+
+
+    $("#stars").text(function () {
+      return $(this).text().replace("...", worldTotal); 
+    });
+    
+    $(".stars").append(function () {
+      let levelNum = $(this).attr('id').match(/\d+/)[0];
+      let images = []
+      
+      for (let i = 0; i < 3; i++) {
+        if (numStars[levelNum] !== undefined && i < numStars[levelNum]) {
+          images.push("<img id='"+i+"' src='./game/assets/star.png'/>")
+        } else {
+          images.push("<img id='"+i+"' src='./game/assets/emptyStar.png'/>")
+        }
+      }
+      return $(this).html(images[0] + images[1] + images[2])
+    })
+  }
 });
