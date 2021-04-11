1844c46d3d5294611f71f7b1e7d5f5e424f5e7ca
Narducky <duckynar@gmail.com>
2021-04-11 17:26:49 -0500

    New sound effect
    New sound effect for game.

--- public/user/game/behaviors/TextControllerOld.js
+++ public/user/game/behaviors/TextControllerOld.js
@@ -4,7 +4,7 @@
 
 import Base from "../../engine/Base.js"
 
-export default class TextController extends Base.Behavior {
+export default class TextControllerOld extends Base.Behavior {
     /*
 		constructor
 		params: timer - A reference to the game's Timer behavior; text - A reference to the game's guideText's TextComponent
@@ -36,19 +36,19 @@ export default class TextController extends Base.Behavior {
     update() {
         //The player has not started tapping yet
         if (this.timer.startTime == -1) {
-            this.text.text = "Press Spacebar to fire the laser!";
+            this.text.text = "Press Spacebar in time with the beat!";
             //The sound is fading in (During first half of soundOn phases excluding the first)
         } else if (this.timer.currentCycle != 1 && this.timer.currentTime < this.timer.startTime + this.timer.soundPhaseTime / 2) {
             this.text.text = "The sound is coming back. Use this time to get back on beat!";
             //The sound is steady (During first 3/4 of soundOn phases, unless fading in)
         } else if (this.timer.currentTime < this.timer.startTime + this.timer.soundPhaseTime / 4 * 3) {
-            this.text.text = "The UFO's will soon become invisible. Keep firing in rythm!";
+            this.text.text = "The sound will soon fade out. Keep tapping after the sound fades out!";
             //The sound is fading out (During last 1/4 of soundOn phases)
         } else if (this.timer.currentTime < this.timer.startTime + this.timer.soundPhaseTime) {
-            this.text.text = "The UFO's are disappearing. Keep firing!";
+            this.text.text = "The sound is fading out. Keep Tapping!";
             //The sound is off (During soundOff phases)
         } else {
-            this.text.text = "Keep firing!";
+            this.text.text = "Keep tapping!";
         }
     }
 }
--- public/user/game/behaviors/Timer.js
+++ public/user/game/behaviors/Timer.js
@@ -51,7 +51,7 @@ export default class Timer extends Base.Behavior {
     */
     start() {
         this.scoreCalculator = this.gameObject.getComponent(ScoreCalculator);
-        this.beatSound = new Audio("./game/assets/newbeat.wav");
+        this.beatSound = new Audio("./game/assets/scifibeat.mp3");
 
         //Algorithm to determine the amount of volume change each beat once 3/4 of the way through soundPhase
         this.volumeChange = 1 / (((0.25 * (this.soundPhaseTime / 1000))) / (this.beatTime / 1000));
@@ -71,7 +71,7 @@ export default class Timer extends Base.Behavior {
         // MIGHT NEED TO CHANGE THE TIMING OF THE FADE OUT
         let target = new Base.GameObject(100, 145);
         let img = new Image()
-        if (this.currentTime >= (this.startTime + (0.75 * this.soundPhaseTime)) && this.startTime != -1) {
+        if (this.currentTime >= (this.startTime + (0.99 * this.soundPhaseTime)) && this.startTime != -1) {
             img.src = "./game/assets/UFO/UFOCapstoneTransparent.png";
         }
         else if (this.currentTime >= (this.startTime + (0.50 * this.soundPhaseTime)) && this.startTime != -1) {
